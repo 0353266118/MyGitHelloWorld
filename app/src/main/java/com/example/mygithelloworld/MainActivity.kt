6fc1da861c7bb163337baa17f24bb2f20ee07798
0353266118 <trinhkhanhduy001@gmail.com>
2025-06-09 00:07:52 +0700

    FEAT: Update welcome message for Compose app and add main activity layout

--- app/src/main/java/com/example/mygithelloworld/MainActivity.kt
+++ app/src/main/java/com/example/mygithelloworld/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello, Git! Welcome to My Compose Project!",
         modifier = modifier
     )
 }
